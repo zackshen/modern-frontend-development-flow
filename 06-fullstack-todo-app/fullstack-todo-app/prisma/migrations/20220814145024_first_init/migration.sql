e8f8f19a1487f9191a4d17cc105846d4faac2a10
Zack Shen <zackshen0526@gmail.com>
2022-08-16 23:50:40 +0800

    数据库改成postgresql

--- 06-fullstack-todo-app/fullstack-todo-app/prisma/migrations/20220814145024_first_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "content" TEXT NOT NULL
-);
---
+++ 06-fullstack-todo-app/fullstack-todo-app/prisma/migrations/20220816154926_initialize/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
